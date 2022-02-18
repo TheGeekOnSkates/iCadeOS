47e961970452892e04751034c4cc91caa1222e88
The Geek on Skates <geekonskates@protonmail.com>
2022-02-17 23:59:18 -0500

    Found & added noise generators from a previous project
    It's not connected to anything yet, but it works (lol).
    I created it a few years ago based on this tutorial:
    
    https://noisehack.com/generate-noise-web-audio-api/
    
    Next I need to wire it up to some RAM; I'm thinking 4 noise generators,
    where the 2 high bits are the type of noise (white, brown or pink),
    and the lower 6 bits are the volume (0-50, each 1 being 2% volume to get 100).

--- sound.js
+++ sound.js
@@ -90,6 +90,78 @@ var sound = {
 	},
 	
 	/**
+	 * Creates a "white noise" generator
+	 * @return A custom audio component that can be
+	 * connected to a volume ("gain") node
+	 */
+	WhiteNoise: function() {
+		var bufferSize = 2 * sound.audio.sampleRate,
+			noiseBuffer = sound.audio.createBuffer(1, bufferSize, sound.audio.sampleRate),
+			output = noiseBuffer.getChannelData(0);
+		for (var i = 0; i < bufferSize; i++) {
+			output[i] = Math.random() * 2 - 1;
+		}
+		var whiteNoise = sound.audio.createBufferSource();
+		whiteNoise.buffer = noiseBuffer;
+		whiteNoise.loop = true;
+		whiteNoise.start(0);
+		return whiteNoise;
+	},
+
+	/**
+	 * Creates a "pink noise" generator
+	 * @return A custom audio component that can be
+	 * connected to a volume ("gain") node
+	 */
+	PinkNoise: function() {
+		var bufferSize = 4096;
+		return (function() {
+			var b0, b1, b2, b3, b4, b5, b6;
+			b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+			var node = sound.audio.createScriptProcessor(bufferSize, 1, 1);
+			node.onaudioprocess = function(e) {
+				var output = e.outputBuffer.getChannelData(0);
+				for (var i = 0; i < bufferSize; i++) {
+					var white = Math.random() * 2 - 1;
+					b0 = 0.99886 * b0 + white * 0.0555179;
+					b1 = 0.99332 * b1 + white * 0.0750759;
+					b2 = 0.96900 * b2 + white * 0.1538520;
+					b3 = 0.86650 * b3 + white * 0.3104856;
+					b4 = 0.55000 * b4 + white * 0.5329522;
+					b5 = -0.7616 * b5 - white * 0.0168980;
+					output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+					output[i] *= 0.11; // (roughly) compensate for gain
+					b6 = white * 0.115926;
+				}
+			}
+			return node;
+		})();
+	},
+
+	/**
+	 * Creates a "brown noise" generator
+	 * @return A custom audio component that can be
+	 * connected to a volume ("gain") node
+	 */
+	BrownNoise: function () {
+		var bufferSize = 4096;
+		return (function() {
+			var lastOut = 0.0;
+			var node = sound.audio.createScriptProcessor(bufferSize, 1, 1);
+			node.onaudioprocess = function(e) {
+				var output = e.outputBuffer.getChannelData(0);
+				for (var i = 0; i < bufferSize; i++) {
+					var white = Math.random() * 2 - 1;
+					output[i] = (lastOut + (0.02 * white)) / 1.02;
+					lastOut = output[i];
+					output[i] *= 3.5; // (roughly) compensate for gain
+				}
+			}
+			return node;
+		})();
+	},
+	
+	/**
 	 * Resets all sound settings to zero
 	 * @param {Uint8Array} ram The memoey used to store sound settings
 	 */
